c5f515f126505c01f7b93894f3338f5224abb240
IgorDerk <derk.i26252@gmail.com>
2024-12-17 21:46:49 +0100

    HW 19

---
+++ homeworks/homework_19/script.js
@@ -0,0 +1,35 @@
+const gridProducts = document.querySelector("#grid-products");
+
+async function fetchProducts() {
+  const res = await fetch("https://dummyjson.com/products");
+  const data = await res.json();
+  data.products.map((product) => {
+    const card = document.createElement("div");
+    card.className = "product-card";
+    card.classList.add("primary");
+
+    const heading = document.createElement("h4");
+    heading.textContent = product.title;
+
+    const img = document.createElement("img");
+    img.src = product.images[0];
+    img.alt = product.title;
+
+    const wrapper = document.createElement("div");
+    wrapper.className = "img-wrapper";
+    wrapper.append(img);
+
+    const desc = document.createElement("p");
+
+    let descText = product.description;
+    desc.textContent = product.description;
+
+    const price = document.createElement("p");
+    price.textContent = `Price: ${product.price}€`;
+
+    card.append(heading, price, wrapper, desc);
+    gridProducts.append(card);
+  });
+}
+
+fetchProducts();
